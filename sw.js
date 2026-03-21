0eba5bc4d18b72bab89426b18a2c5890a2d56710
Shane-Whelan413 <Whelan.Shane@hotmail.com>
2026-03-21 03:41:49 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'brayhead-gs-v2';
+const ASSETS = [
+  '/brayhead-golf/index.html',
+  '/brayhead-golf/manifest.json',
+  '/brayhead-golf/icon-192.png',
+  '/brayhead-golf/icon-512.png',
+  'https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600&family=DM+Serif+Display&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.url.includes('supabase.co')) return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
