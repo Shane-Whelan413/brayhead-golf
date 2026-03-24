f0ab17e0dff29de5b144f149501122b243131426
Shane-Whelan413 <Whelan.Shane@hotmail.com>
2026-03-24 19:35:34 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,24 @@
-const CACHE = 'brayhead-gs-v2';
+const CACHE = 'brayhead-gs-v4';
 const ASSETS = [
   '/brayhead-golf/index.html',
   '/brayhead-golf/manifest.json',
   '/brayhead-golf/icon-192.png',
   '/brayhead-golf/icon-512.png',
-  'https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600&family=DM+Serif+Display&display=swap'
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+    caches.open(CACHE)
+      .then(c => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
 
